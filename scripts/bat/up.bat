cd4b301be06a8d70307480066ce67294677e3203
Rupinder Singh <rupinder@nibl.tech>
2024-10-30 01:33:17 -0400

    Uodated the up script to remove tempo-data on startup

--- scripts/bat/up.bat
+++ scripts/bat/up.bat
@@ -3,6 +3,7 @@
 set SCRIPTS_DIR=%~dp0
 
 rmdir /S/Q %SCRIPTS_DIR%\..\..\otel\logs
+rmdir /S/Q %SCRIPTS_DIR%\..\..\grafana\tempo-data
 
 :: Bring up the Docker containers
 %SCRIPTS_DIR%compose.bat up -d %* 
