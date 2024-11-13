a5035939038e296340bfcbd7ae748354994b526f
Rupinder Singh <rupinder@nibl.tech>
2024-11-13 08:48:49 -0500

    Removed tempo-data folder before starting

--- scripts/sh/up.sh
+++ scripts/sh/up.sh
@@ -3,6 +3,7 @@
 SCRIPTS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 rm -rf "${SCRIPTS_DIR}"/../../otel/logs
+rm -rf "${SCRIPTS_DIR}"/../../grafana/tempo-data
 
 # Bring up the Docker containers
 ${SCRIPTS_DIR}/compose.sh up -d $*
