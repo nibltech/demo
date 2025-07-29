925d6de5f41ff04c771c28e8e379018efdb47ed3
Rupinder Singh <rupinder@nibl.tech>
2025-07-28 22:41:53 -0400

    Changed data.sh script to run newman or the API client based on input

--- scripts/sh/data.sh
+++ scripts/sh/data.sh
@@ -4,7 +4,7 @@ SCRIPTS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 if [ "${1}" == "consumer" ]; then
   # Run the APi Consumer container to submit requests to API
-  ${SCRIPTS_DIR}/compose.sh run -d api-consumer
+  ${SCRIPTS_DIR}/compose.sh run -d api-consumer --name api-consumer
 else
   # Run the Nibble Demo API Postman collection 100 times waiting 1 second in between each request
   ${SCRIPTS_DIR}/compose.sh run -d newman run nibble-demo-api.postman_collection.json -n 100 --delay-request 1000
