7b9661678677df394518cbc2cddd5caa924caf20
Rupinder Singh <rupinder@nibl.tech>
2025-08-28 16:07:37 -0500

    Updated build.bat file to include the build for the ApiConsumer service

--- scripts/bat/build.bat
+++ scripts/bat/build.bat
@@ -6,6 +6,10 @@ pushd %SCRIPTS_DIR%\..\..\spring\Orders
 call ..\gradlew.bat -x test copyDependencies build
 popd
 
+pushd %SCRIPTS_DIR%\..\..\spring\ApiConsumer
+call ..\gradlew.bat -x test copyDependencies build
+popd
+
 :: Build the Docker images
 set SAG_HOME=
 %SCRIPTS_DIR%compose.bat build %*
