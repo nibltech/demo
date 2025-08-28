5cad4b5227b2d49e4e24062805525e2f38a62cef
Rupinder Singh <rupinder@nibl.tech>
2025-08-28 12:31:37 -0500

    Updated build.bat file to use correct path to gradle

--- scripts/bat/build.bat
+++ scripts/bat/build.bat
@@ -3,7 +3,7 @@
 set SCRIPTS_DIR=%~dp0
 
 pushd %SCRIPTS_DIR%\..\..\spring\Orders
-call gradlew.bat -x test copyDependencies build
+call ..\gradlew.bat -x test copyDependencies build
 popd
 
 :: Build the Docker images
