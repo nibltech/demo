eb993aeca660f029422466633e3d5879e5339393
Rupinder Singh <rupinder@nibl.tech>
2025-08-28 19:02:11 -0500

    Added the remove orphans option to cleanup orphans from previous runs

--- scripts/bat/data.bat
+++ scripts/bat/data.bat
@@ -5,8 +5,8 @@ set SCRIPTS_DIR=%~dp0
 REM Check if the first argument is "consumer"
 if "%1"=="consumer" (
     REM Run the API Consumer container
-    "%SCRIPTS_DIR%\compose.bat" run -d api-consumer --name api-consumer
+    "%SCRIPTS_DIR%\compose.bat" run --remove-orphans -d api-consumer --name api-consumer
 ) else (
     REM Run the Nibble Demo API Postman collection 100 times with 1 second delay
-    "%SCRIPTS_DIR%\compose.bat" run -d newman run nibble-demo-api.postman_collection.json -n 100 --delay-request 1000
+    "%SCRIPTS_DIR%\compose.bat" run --remove-orphans -d newman run nibble-demo-api.postman_collection.json -n 100 --delay-request 1000
 )
