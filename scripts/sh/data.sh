a57452df3f35f64f8d1699f36a42eaba51904c85
Rupinder Singh <rupinder@nibl.tech>
2025-08-28 19:00:21 -0500

    Added the remove orphans option to cleanup orphans from previous runs

--- scripts/sh/data.sh
+++ scripts/sh/data.sh
@@ -4,8 +4,8 @@ SCRIPTS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 if [ "${1}" == "consumer" ]; then
   # Run the APi Consumer container to submit requests to API
-  ${SCRIPTS_DIR}/compose.sh run -d api-consumer --name api-consumer
+  ${SCRIPTS_DIR}/compose.sh run --remove-orphans -d api-consumer --name api-consumer
 else
   # Run the Nibble Demo API Postman collection 100 times waiting 1 second in between each request
-  ${SCRIPTS_DIR}/compose.sh run -d newman run nibble-demo-api.postman_collection.json -n 100 --delay-request 1000
+  ${SCRIPTS_DIR}/compose.sh run --remove-orphans -d newman run nibble-demo-api.postman_collection.json -n 100 --delay-request 1000
 fi
